5cc5091e30c1b6004a0b4ba667ffa73c77f9b4f4
叶同学 <jinzhanye9@gmail.com>
2020-01-25 15:05:30 +0800

    添加 loader 笔记

--- my-pack-test/my-webpack.config.js
+++ my-pack-test/my-webpack.config.js
@@ -22,40 +22,13 @@ module.exports = {
     filename: 'bundle.js',
     path: path.resolve(__dirname, 'dist'),
   },
-  resolveLoader: {
-    modules: ['node_modules', path.resolve(__dirname, 'loaders')]
-    // 另一种方式使用别名 alias: {}
-  },
   module: {
-    // pre + normal + inline + post
     rules: [{
       test: /\.less$/,
       use: [
         path.resolve(__dirname, 'loaders', 'style-loader'),
         path.resolve(__dirname, 'loaders', 'less-loader'),
       ]
-    }, {
-      test: /\.js$/,
-      use: {
-        loader: 'test-loader1'
-      },
-      enforce: 'pre'
-    }, {
-      test: /\.js$/,
-      use: {
-        loader: 'test-loader2'
-      }
-    }, {
-      test: /\.js$/,
-      use: {
-        loader: 'test-loader3'
-      },
-    }, {
-      test: /\.js$/,
-      use: {
-        loader: 'test-loader4'
-      },
-      enforce: 'post'
     }]
   },
   plugins: [
--- my-pack-test/src/a.js
+++ my-pack-test/src/a.js
@@ -1,3 +1,9 @@
-const b = require('./base/b')
+// 前置 -! 不让文件执行 pre、normal loader
+// 前置 ! 不让文件执行 normal loader
+// 前置 !! 只执行行内 loader，其他 loader 都不执行
+// 后置 ! 执行行内 loader
+const b = require('!!inline-loader!./base/b')
 
 module.exports = b + 'a'
+
+// loader 分为 pitch loader、normal loader
